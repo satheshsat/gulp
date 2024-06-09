9c82a4635114b2e115c912272cdc6361219caf20
Sathesh kumar <satheshs.sat@gmail.com>
2024-06-09 13:35:37 +0530

    Concat output to single file

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('autoprefixer');
 var postcss = require('gulp-postcss');
 const cleanCSS = require("gulp-clean-css");
 const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 
 function clean(cb) {
   // body omitted
@@ -16,6 +17,7 @@ function css(cb) {
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(cleanCSS())
+    .pipe(concat('app.css'))
     .pipe(dest('output/css'));
 }
 
@@ -23,20 +25,22 @@ function javascript(cb) {
     return src('js/**/*.js')
     .pipe(babel())
     .pipe(uglify())
+    .pipe(concat('app.js'))
     .pipe(dest('output/js'));
 }
 
 function devcss(cb) {
     return src('css/**/*.css')
     .pipe(sass().on('error', sass.logError))
-    .pipe(babel())
     .pipe(postcss([autoprefixer()]))
+    .pipe(concat('app.css'))
     .pipe(dest('output/css'));
 }
 
 function devjavascript(cb) {
     return src('js/**/*.js')
     .pipe(babel())
+    .pipe(concat('app.js'))
     .pipe(dest('output/js'));
 }
 
